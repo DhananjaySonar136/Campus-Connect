b33f53230eb485e0e72ad453295fd89f2d4c2aa9
DhananjaySonar136 <sonar8187@gmail.com>
2026-05-08 16:38:48 +0530

    url updated

--- mobile/src/constants/config.ts
+++ mobile/src/constants/config.ts
@@ -1,7 +1,6 @@
-// export const API_BASE_URL = 'http://172.17.127.167:8080/api';
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://172.17.127.167:8080/api';
 export const emulatorApiHints = {
   androidEmulator: 'http://10.0.2.2:8080/api',
   iosSimulator: 'http://localhost:8080/api',
-  physicalDevice: 'http://YOUR_COMPUTER_LAN_IP:8080/api'
+  physicalDevice: 'http://172.17.127.167:8080/api'
 };
